ef8512d9d7944243eb3df3e41c594b01af57ac00
smileclj <842705148@qq.com>
2015-12-24 15:42:30 +0800

    logback 日志

---
+++ src/main/java/log/LogTest.java
@@ -0,0 +1,18 @@
+package log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by wana on 2015/12/24.
+ */
+public class LogTest {
+    private static final Logger logger = LoggerFactory.getLogger(LogTest.class);
+
+    public static void main(String[] args) {
+//        for(int i = 0;i<50;i++){
+            logger.info("info");
+//        }
+
+    }
+}
--- src/main/java/netty/official/TimeServerHandler.java
+++ src/main/java/netty/official/TimeServerHandler.java
@@ -4,12 +4,18 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.nio.charset.Charset;
 import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class TimeServerHandler extends ChannelHandlerAdapter {
     private int counter;
+    private Logger logger = LoggerFactory.getLogger(getClass());
+    private ExecutorService pool = Executors.newFixedThreadPool(8);
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
@@ -18,12 +24,23 @@ public class TimeServerHandler extends ChannelHandlerAdapter {
 //        byte[] req = new byte[buf.readableBytes()];
 //        buf.readBytes(req);
 //        String body = new String(req, Charset.forName("UTF-8")).substring(0,req.length - System.getProperty("line.separator").length());
-        String body = (String) msg;
-        System.out.println("the time server receive order:" + body + ";the counter is :" + ++counter);
-        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date(System.currentTimeMillis()).toString() : "BAD ORDER";
-        currentTime = currentTime + System.getProperty("line.separator");
-        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
-        ctx.writeAndFlush(resp);
+        pool.execute(new Runnable() {
+            @Override
+            public void run() {
+                logger.info(Thread.currentThread().getName());
+                String body = (String) msg;
+//        System.out.println("the time server receive order:" + body + ";the counter is :" + ++counter);
+                String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+                currentTime = currentTime + System.getProperty("line.separator");
+                ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                ctx.writeAndFlush(resp);
+            }
+        });
     }
 
     @Override
